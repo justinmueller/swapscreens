f0582ae0ee37c6a253c38ecc4d575808e3164b32
Justin Mueller <justin@justinmueller.com>
2017-09-15 11:41:01 -0500

    Added code.

--- Swap Screens/main.swift
+++ Swap Screens/main.swift
@@ -8,5 +8,27 @@
 
 import Foundation
 
-print("Hello, World!")
+// Start display config
+var config:CGDisplayConfigRef? = nil;
+CGBeginDisplayConfiguration(&config);
 
+// Get the displays
+let maxDisplays: UInt32 = 3;
+var screens = [CGDirectDisplayID](repeating: 0, count: Int(maxDisplays));
+var displayCount: UInt32 = 0;
+
+CGGetOnlineDisplayList(maxDisplays, &screens, &displayCount);
+
+// Handle the displays
+for currentDisplay in screens {
+    var bounds = CGDisplayBounds(currentDisplay);
+    if (Int(bounds.origin.x) == -1920) {
+        CGConfigureDisplayOrigin(config,currentDisplay, 1440, Int32(bounds.origin.y));
+    } else if (Int(bounds.origin.x) == 1440) {
+        CGConfigureDisplayOrigin(config,currentDisplay, -1920, Int32(bounds.origin.y));
+    }
+}
+
+// Commit the change
+CGCompleteDisplayConfiguration(config, CGConfigureOption.permanently);
+print("Screens have been swapped.");
